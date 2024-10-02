45183fbfc4c3a8573834fd4de50539fed46fa500
gabrieldeoa <gabrielaraujo.dev@gmail.com>
2024-10-02 20:04:29 -0300

    chore: increase quantity of characters by commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,6 +6,6 @@ module.exports = {
       "always",
       ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
     ],
-    "header-max-length": [2, "always", 72],
+    "header-max-length": [2, "always", 80],
   },
 };
