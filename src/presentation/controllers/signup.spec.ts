0f0e6f10751287bf9ae9372fca7a711751fd6c12
gabrieldeoa <gabrielaraujo.dev@gmail.com>
2024-10-06 06:23:20 -0300

    test: ensure SignUpController call EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -104,4 +104,23 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError("email"));
   });
+
+  test("Should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+    const validEmail = "any_email@email.com";
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: validEmail,
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith(validEmail);
+  });
 });
